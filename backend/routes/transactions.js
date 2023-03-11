ff943a0ba6efbc7e03112362d0eda93ff85ba53c
Miguel A. Ledesma <miguelledesma33@gmail.com>
2023-03-10 20:00:24 -0500

    added comments to routes

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -8,6 +8,8 @@ const router = require("express").Router();
 
 //post method to add income
 router.post("/add-income", addIncome);
+//endpoint to retrieve incomes in database
 router.get("/get-income", getIncome);
+//endpoint to delete income by id
 router.delete("/delete-income", deleteIncome);
 module.exports = { router };
