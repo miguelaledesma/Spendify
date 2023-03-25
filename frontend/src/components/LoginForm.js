40e50c9fe3e40dc511d56ab4a63d5418d2b6066d
Miguel A. Ledesma <miguelledesma33@gmail.com>
2023-03-25 19:26:24 -0400

    created login form and edited signup form

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-const SignUpForm = () => {
+const LoginForm = () => {
   return (
     <div class="">
       <section class="bg-gray-50 dark:bg-gray-900 h-screen">
@@ -12,25 +12,9 @@ const SignUpForm = () => {
           <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700  my-12 sm:my-8 ">
             <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Create an account
+                Welcome back!
               </h1>
               <form class="space-y-4 md:space-y-6" action="#">
-                <div>
-                  <label
-                    for="username"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Username
-                  </label>
-                  <input
-                    type="text"
-                    name="username"
-                    id="username"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Username"
-                    required=""
-                  />
-                </div>
                 <div>
                   <label
                     for="email"
@@ -94,15 +78,15 @@ const SignUpForm = () => {
                   type="submit"
                   class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                 >
-                  Sign up
+                  Login
                 </button>
                 <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                  Have an account?{" "}
+                  Don't have an account?{" "}
                   <Link
-                    to="/login"
+                    to="/signup"
                     class="font-medium text-primary-600 hover:underline dark:text-primary-500"
                   >
-                    Login
+                    Sign Up
                   </Link>
                 </p>
               </form>
@@ -114,4 +98,4 @@ const SignUpForm = () => {
   );
 };
 
-export default SignUpForm;
+export default LoginForm;
