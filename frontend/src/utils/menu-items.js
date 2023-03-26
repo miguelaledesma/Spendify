e6e75b0374e99fbff8d4c323a3de403f0a5498c5
Miguel A. Ledesma <miguelledesma33@gmail.com>
2023-03-26 15:04:05 -0400

    added menuitems to utils folder

--- frontend/src/utils/menu-items.js
+++ frontend/src/utils/menu-items.js
@@ -1,1 +1,28 @@
 //add menu items that will be used in the dashboard here.
+import { dashboard, transactions, trend, expenses } from "./icons";
+export const menuItems = [
+  {
+    id: 1,
+    title: "Dashboard",
+    icon: dashboard,
+    link: "/dashboard",
+  },
+  {
+    id: 2,
+    title: "View Transactions",
+    icon: transactions,
+    link: "/dashboard",
+  },
+  {
+    id: 3,
+    title: "Incomes",
+    icon: trend,
+    link: "/dashboard",
+  },
+  {
+    id: 4,
+    title: "Expenses",
+    icon: expenses,
+    link: "/dashboard",
+  },
+];
