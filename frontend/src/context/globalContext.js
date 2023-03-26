d4cc031bb93d237ec32c282a18b12e2c2823071c
Miguel A. Ledesma <miguelledesma33@gmail.com>
2023-03-26 15:05:47 -0400

    added globalContext, updated login form to handle login, dashboard accesses by login

---
+++ frontend/src/context/globalContext.js
@@ -0,0 +1,142 @@
+import React, { useContext, useState, useEffect } from "react";
+import axios from "axios";
+
+const BASE_URL = "http://localhost:5000/api/v1/";
+
+const GlobalContext = React.createContext();
+
+export const GlobalProvider = ({ children }) => {
+  const [incomes, setIncomes] = useState([]);
+  const [expenses, setExpenses] = useState([]);
+  const [error, setError] = useState(null);
+  const [user, setUser] = useState(null);
+
+  //   const fetchCurrentUser = async (id) => {
+  //     try {
+  //       const response = await axios.get(`${BASE_URL}user:${id}`);
+  //       console.log(response.data.user);
+  //       setUser(response.data.user);
+  //     } catch (error) {
+  //       throw new Error(error.response.data.message);
+  //     }
+  //   };
+
+  //   useEffect(() => {
+  //     const token = localStorage.getItem("token");
+  //     if (token) {
+  //       axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  //       fetchCurrentUser();
+  //     }
+  //   }, []);
+  //calculate incomes
+
+  const loginUser = async (email, password) => {
+    try {
+      const response = await axios.post(`${BASE_URL}login`, {
+        email,
+        password,
+      });
+      const { user, token } = response.data;
+      localStorage.setItem("token", token);
+      console.log(response.data);
+      return user;
+    } catch (error) {
+      throw new Error(error.response.data.message);
+    }
+  };
+  const addIncome = async (income) => {
+    const response = await axios
+      .post(`${BASE_URL}add-income`, income)
+      .catch((err) => {
+        setError(err.response.data.message);
+      });
+    getIncomes();
+  };
+
+  const getIncomes = async () => {
+    const response = await axios.get(`${BASE_URL}get-income`);
+    setIncomes(response.data);
+    console.log(response.data);
+  };
+
+  const deleteIncome = async (id) => {
+    const res = await axios.delete(`${BASE_URL}delete-income/${id}`);
+    getIncomes();
+  };
+
+  const totalIncome = () => {
+    let totalIncome = 0;
+    incomes.forEach((income) => {
+      totalIncome = totalIncome + income.amount;
+    });
+
+    return totalIncome;
+  };
+
+  //calculate incomes
+  const addExpense = async (income) => {
+    const response = await axios
+      .post(`${BASE_URL}add-expense`, income)
+      .catch((err) => {
+        setError(err.response.data.message);
+      });
+    getExpenses();
+  };
+
+  const getExpenses = async () => {
+    const response = await axios.get(`${BASE_URL}expenses`);
+    setExpenses(response.data);
+    console.log(response.data);
+  };
+
+  const totalExpenses = () => {
+    let totalIncome = 0;
+    expenses.forEach((income) => {
+      totalIncome = totalIncome + income.amount;
+    });
+
+    return totalIncome;
+  };
+
+  const totalBalance = () => {
+    return totalIncome() - totalExpenses();
+  };
+
+  const transactionHistory = () => {
+    const history = [...incomes, ...expenses];
+    history.sort((a, b) => {
+      return new Date(b.createdAt) - new Date(a.createdAt);
+    });
+
+    return history.slice(0, 3);
+  };
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        addIncome,
+        getIncomes,
+        incomes,
+        deleteIncome,
+        expenses,
+        totalIncome,
+        addExpense,
+        getExpenses,
+        totalExpenses,
+        totalBalance,
+        transactionHistory,
+        error,
+        setError,
+        user,
+        setUser,
+        loginUser,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(GlobalContext);
+};
