16573855325324bceef0b81e841ef380d23eee61
Miguel A. Ledesma <miguelledesma33@gmail.com>
2023-03-30 19:14:16 -0400

    added userId to Expense Schema and updated route function to add expense

--- backend/controllers/expenseController.js
+++ backend/controllers/expenseController.js
@@ -1,31 +1,19 @@
 const ExpenseSchema = require("../models/ExpenseModel");
 
 exports.addExpense = async (req, res) => {
-  const { title, amount, category, description, date } = req.body;
-  //create a new instance of the ExpenseSchema, pass in the properties from the request body
-
-  const expense = ExpenseSchema({
-    title,
-    amount,
-    category,
-    description,
-    date,
-  });
-  //sending to database using try/catch (need validations first)
   try {
-    //validations, need all fields to be filled out, amount needs to be greater than 0 to be a valid entry.
-    if (!title || !amount || !category || !description || !date) {
-      res.status(400).json({
-        error:
-          "all fields are required. eg, title, amount, category, description, date",
-      });
-    }
-    if (isNaN(amount) && amount <= 0) {
-      res.status(400).json({
-        error:
-          "amount needs to be greater than 0, amount needs to be a positive number",
-      });
-    }
+    const { title, amount, category, description, date } = req.body;
+    //create a new instance of the ExpenseSchema, pass in the properties from the request body
+
+    const expense = ExpenseSchema({
+      userId: req.user._id,
+      title,
+      amount,
+      category,
+      description,
+      date,
+    });
+
     await expense.save();
     res.status(200).json({
       message: "Expense successfully added!",
@@ -37,4 +25,20 @@ exports.addExpense = async (req, res) => {
     });
   }
   console.log(expense);
+};
+
+exports.getExpenses = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const userExpenses = await ExpenseSchema.find({ userId }).sort({
+      createdAt: -1,
+    });
+    res.status(200).json(userExpenses);
+  } catch (error) {
+    res.status(500).json({
+      message:
+        "Error retrieving expenses for the user, check database or make sure user is authenticated",
+      error: error.message,
+    });
+  }
 };
--- backend/models/ExpenseModel.js
+++ backend/models/ExpenseModel.js
@@ -4,6 +4,11 @@ const mongoose = require("mongoose");
 
 const ExpenseSchema = new mongoose.Schema(
   {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
     title: {
       type: String,
       require: true,
