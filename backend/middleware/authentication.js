4d73deb28e1c8b821b26e0ab32ad30c97244cb8f
Miguel A. Ledesma <miguelledesma33@gmail.com>
2023-04-07 14:30:26 -0400

    fixed getIncome for user

--- backend/middleware/authentication.js
+++ backend/middleware/authentication.js
@@ -14,6 +14,7 @@ const auth = async (req, res, next) => {
     }
     req.token = token;
     req.user = user;
+    // console.log(req.user);
     next();
   } catch (error) {
     console.log(error);
