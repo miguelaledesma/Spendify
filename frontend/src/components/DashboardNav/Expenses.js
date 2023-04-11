c96ce8f215b98f817c52713960f5aecb9637d8a2
Miguel A. Ledesma <miguelledesma33@gmail.com>
2023-04-10 21:52:16 -0400

    working on functionality and styles for form

--- frontend/src/components/DashboardNav/Expenses.js
+++ frontend/src/components/DashboardNav/Expenses.js
@@ -1,11 +1,16 @@
 import React, { useEffect } from "react";
 import { useGlobalContext } from "../../context/globalContext";
+import ExpenseForm from "../Forms/ExpenseForm";
 const Expense = () => {
   const { getExpenses } = useGlobalContext();
   useEffect(() => {
     getExpenses();
   }, []);
-  return <div>Expense</div>;
+  return (
+    <div>
+      <ExpenseForm />
+    </div>
+  );
 };
 
 export default Expense;
