0ce5be9d9e633f7814da86991f03d95de62c590c
Miguel A. Ledesma <miguelledesma33@gmail.com>
2023-04-10 21:39:43 -0400

    working on fixing globalContext functions

--- frontend/src/components/DashboardNav/Expenses.js
+++ frontend/src/components/DashboardNav/Expenses.js
@@ -1,4 +1,10 @@
+import React, { useEffect } from "react";
+import { useGlobalContext } from "../../context/globalContext";
 const Expense = () => {
+  const { getExpenses } = useGlobalContext();
+  useEffect(() => {
+    getExpenses();
+  }, []);
   return <div>Expense</div>;
 };
 
