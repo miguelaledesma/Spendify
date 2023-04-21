39d30506760c26fa5d42a05635d14bb2a13f84cd
Miguel A. Ledesma <miguelledesma33@gmail.com>
2023-04-21 18:57:03 -0400

    responsive chart update and history component update

--- frontend/src/components/DashboardNav/Items/Chart.js
+++ frontend/src/components/DashboardNav/Items/Chart.js
@@ -61,20 +61,19 @@ function FinanceChart() {
   };
 
   return (
-    <ChartStyled>
-      {" "}
+    <div className="chart-container" style={{}}>
       <Line data={data} />
-    </ChartStyled>
+    </div>
   );
 }
 
 export default FinanceChart;
 
-const ChartStyled = styled.div`
-  background: #fcf6f9;
-  border: 2px solid #ffffff;
-  box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
-  padding: 1rem;
-  border-radius: 20px;
-  height: 100%;
-`;
+// const ChartStyled = styled.div`
+//   background: #fcf6f9;
+//   border: 2px solid #ffffff;
+//   box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+//   padding: 1rem;
+//   border-radius: 20px;
+//   height: 100%;
+// `;
