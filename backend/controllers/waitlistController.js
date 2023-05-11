de49c9b42b673f3de50fc81680a303617a42ce0e
Miguel A. Ledesma <miguelledesma33@gmail.com>
2023-05-11 15:17:29 -0400

    updated backend functionality to check for existing user in waitlist

--- backend/controllers/waitlistController.js
+++ backend/controllers/waitlistController.js
@@ -4,7 +4,12 @@ exports.addToWaitList = async (req, res) => {
   try {
     const { email } = req.body;
     //create a new instance of the ExpenseSchema, pass in the properties from the request body
+    const existingUserOnWaitList = await WaitlistSchema.findOne({ email });
+    if (existingUserOnWaitList) {
+      res.status(400).json({ message: "Email already on the waitlst" });
 
+      return;
+    }
     const userOnWaitList = new WaitlistSchema({
       email,
     });
