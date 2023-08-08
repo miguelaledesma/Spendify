c602d7e7d4969799aa4008bc5a67fdddbe474427
Miguel A. Ledesma <miguelledesma33@gmail.com>
2023-08-08 15:03:08 -0700

    removed styling from dashmain

--- frontend/src/components/DashboardMain.js
+++ frontend/src/components/DashboardMain.js
@@ -38,20 +38,3 @@ const DashboardMain = () => {
 };
 
 export default DashboardMain;
-
-//   const AppStyled = styled.div`
-//   height: 100vh;
-//   background-image: url(${props => props.bg});
-//   position: relative;
-//   main{
-//     flex: 1;
-//     background: rgba(252, 246, 249, 0.78);
-//     border: 3px solid #FFFFFF;
-//     backdrop-filter: blur(4.5px);
-//     border-radius: 32px;
-//     overflow-x: hidden;
-//     &::-webkit-scrollbar{
-//       width: 0;
-//     }
-//   }
-// `;
